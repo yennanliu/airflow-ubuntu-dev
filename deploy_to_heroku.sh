45979ce404a6e7456e25d4f2b261c22e441588de
yennanliue <f339339@gmail.com>
2019-07-05 00:39:50 +0800

    fix the requirements.txt location and better the command set up airflow AIRFLOW__CORE__FERNET_KEY

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -7,7 +7,7 @@ set -e
 
 # Install custom python package if requirements.txt is present
 if [ -e "requirements.txt" ]; then
-    $(which pip) install --user -r /requirements.txt
+    $(which pip) install --user -r requirements.txt
 fi
 
 # build heroku project 
@@ -16,9 +16,8 @@ echo 'add postgre and connect to airflow app...'
 heroku addons:create heroku-postgresql:dev -a airflow-heroku
 heroku config:set  -a airflow-heroku  AIRFLOW__CORE__SQL_ALCHEMY_CONN=$(heroku config -a airflow-heroku | grep DATABASE_URL | sed -e 's/ //g' | cut -d  ':' -f 2-)
 heroku config:set  -a airflow-heroku  AIRFLOW__CORE__LOAD_EXAMPLES=False
-f_key=$(python -c "from cryptography.fernet import Fernet; print (Fernet.generate_key())") && f_key_fix="${key##b}"
-heroku config:set  -a airflow-heroku  AIRFLOW__CORE__FERNET_KEY=$(f_key_fix)
-heroku config:set -a airflow-heroku AIRFLOW__WEBSERVER__AUTHENTICATE=True
+heroku config:set  -a airflow-heroku  AIRFLOW__CORE__FERNET_KEY=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
+heroku config:set  -a airflow-heroku AIRFLOW__WEBSERVER__AUTHENTICATE=True
 heroku config:set  -a airflow-heroku AIRFLOW__WEBSERVER__AUTH_BACKEND=airflow.contrib.auth.backends.password_auth
 echo 'Airflow Config Vars :\n' && heroku config -a airflow-heroku
 echo 'add heroku remote git...'
