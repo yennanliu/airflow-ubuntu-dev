b6f2b7adfcc7247e18cf65a343b34fe1e41a6368
Yen Liu <yenanliu@amazon.com>
2023-06-26 18:26:00 +0100

    update

--- airflow/dags/test_script.py
+++ airflow/dags/test_script.py
@@ -1,3 +1,4 @@
+import random
 
 def my_func1():
     print ("this is my_func1")
@@ -6,4 +7,12 @@ def my_func2():
     print ("this is my_func2")
 
 def my_func3():
-    return "123"
+    #return "123"
+    return False
+
+def my_func4():
+    num = random.randint(0, 10)
+    if num % 2 == 0:
+        return True
+    else:
+        raise Exception("NOT an even number")
